3b3c019e708f6c1889a797bf314b3b759a570883
Robert Haidl <haidlrob@gmail.com>
2022-05-26 19:17:41 +0200

    bugfix

--- model/Restaurant.js
+++ model/Restaurant.js
@@ -8,7 +8,10 @@ const RestaurantSchema = mongoose.Schema({
         type: String,
         required: true
     },
-
+    index: {
+        type: String,
+        required: false
+    },
     name: {
         type: String,
         required: true
